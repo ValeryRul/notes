87d52636506c1ed6cce2947751248b72bf8119d9
Zwezh <alex.zwezh@gmail.com>
2021-11-15 16:21:38 +0100

    Initial commit

---
+++ src/app/core/shared.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const COMMON_MODULES = [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+];
+
+
+@NgModule({
+    imports: [COMMON_MODULES],
+    exports: [COMMON_MODULES]
+})
+export class SharedModule { }
