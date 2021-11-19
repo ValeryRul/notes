64cd1be32b9fb7a85964ea646fd55a8eb61e130a
ValeryRul <pratatupi@gmail.com>
2021-11-19 12:15:01 +0100

    added registration page with routing and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+/* import { LoginPageComponent } from './modules/auth/pages/login-page/login-page.component';
+ */import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './core/shared.module';
+import { RegistrationPageComponent } from '@appModules/auth/pages/registration-page/registration-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    /* LoginPageComponent, */
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
