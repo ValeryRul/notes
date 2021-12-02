65d3f3b07b4b1abfd63ed1f9498053bcac1ee711
ValeryRul <pratatupi@gmail.com>
2021-12-02 13:53:35 +0100

    deleted duplicate and changed folder structure

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderModule } from '@appModules/header/header.module';
 import { SharedModule } from '@appSharedModule';
-
 import { MainPageComponent } from './main-page/main-page.component';
-import { TodoModule } from './main-page/todo/todo.module';
+import { TodoModule } from '../todo/todo.module';
+import { HeaderModule } from '@appModules/header/header.module';
 
 const routes = [{ path: '', component: MainPageComponent }];
 
 @NgModule({
   declarations: [MainPageComponent],
-  imports: [HeaderModule, TodoModule, SharedModule, RouterModule.forChild(routes)],
-  exports: [MainPageComponent, RouterModule]
+  imports: [HeaderModule, TodoModule, SharedModule, RouterModule.forChild(routes)]
 })
 export class MainModule {}
